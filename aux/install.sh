bec5b428b1b65cf05acbbbf5df2c722b3db6aa94
root <root@localhost.localdomain>
2019-03-31 22:56:06 +0100

    install post-modules script

--- aux/install.sh
+++ aux/install.sh
@@ -36,86 +36,124 @@ cat << !
    |        "install.sh - configuration script"       |
    | Install all post-modules of post_exploitation.rc |
    ╠──────────────────────────────────────────────────╝
-   |_ OS:$OS DISTRO:$DiStRo VERSION:$ver
+   |_ OS:$OS DISTRO:$DiStRo PATH:$IPATH
 
 
 !
 sleep 1
-echo -n "${GreenF}[+]${white} Do you wish to install (y/n): "${Reset}; read op
+echo -n "${GreenF}[+]${white} Do you wish to install post modules (y/n): "${Reset}; read op
 if [ "$op" = "y" ] || [ "$op" = "Y" ]; then
+count="0"
 
     Colors;
     echo ${BlueF}[*] "----------------------------------------"${Reset};
-    echo ${BlueF}[*]${white} "Locate msfdb enum_protections.rb path .."${Reset};
+    echo ${BlueF}[*]${white} "Query msfdb for enum_protections.rb installation .."${Reset};
     aV_path=$(locate modules/post/windows/recon | grep -v '\doc' | grep -v '\documentation' | head -n 1)
-    echo ${BlueF}[*]${white} "Path: $aV_path/enum_protections.rb"${Reset};
+    echo ${YellowF}[i]${white} "Path: $aV_path/enum_protections.rb"${Reset};
     sleep 2
 
        if [ -e "$aV_path/enum_protections.rb" ]; then
-          echo ${GreenF}[*]${white} "Post-module found in msfdb => ${GreenF}(no need to install)"${Reset};
+          echo ${GreenF}[*]${white} "Metasploit Post-module found in msfdb => ${GreenF}(no need to install)"${Reset};
           sleep 2
        else
-          echo ${BlueF}[*]${white} "Copy module to: $aV_path/enum_protections.rb"${Reset};
-          sudo cp $IPATH/enum_protections.rb $aV_path/enum_protections.rb
-          fresh = "yes"
+          echo ${RedF}[x]${white} "Metasploit Post-module NOT found in msfdb."${Reset};
           sleep 2
+          echo ${BlueF}[*]${white} "Downloading Metasploit post-module from github"${Reset};
+          sleep 1
+          echo ""
+          sudo rm -f enum_protections.rb
+          sudo wget https://raw.githubusercontent.com/r00t-3xp10it/resource_files/master/aux/enum_protections.rb
+          echo ${BlueF}[*]${white} "Copy module to: $aV_path/enum_protections.rb"${Reset};
+          sleep 2
+          sudo cp $IPATH/enum_protections.rb $aV_path/enum_protections.rb
+          fresh="yes"
+          count=$(( $count + 1 ))
        fi
 
-    echo ${BlueF}[*]${white} "Locate msfdb SCRNSAVE_T1180_persistence.rb path .."${Reset};
+    echo ${BlueF}[*]${white} "Query msfdb for SCRNSAVE_T1180_persistence.rb installation .."${Reset};
     t1180_path=$(locate modules/post/windows/escalate | grep -v '\doc' | grep -v '\documentation' | head -n 1)
-    echo ${BlueF}[*]${white} "Path: $t1180_path/SCRNSAVE_T1180_persistence.rb"${Reset};
+    echo ${YellowF}[i]${white} "Path: $t1180_path/SCRNSAVE_T1180_persistence.rb"${Reset};
     sleep 2
 
        if [ -e "$t1180_path/SCRNSAVE_T1180_persistence.rb" ]; then
-          echo ${GreenF}[*]${white} "Post-module found in msfdb => ${GreenF}(no need to install)"${Reset};
+          echo ${GreenF}[*]${white} "Metasploit Post-module found in msfdb => ${GreenF}(no need to install)"${Reset};
           sleep 2
        else
-          echo ${BlueF}[*]${white} "Copy module to: $t1180_path/SCRNSAVE_T1180_persistence.rb"${Reset};
-          sudo cp $IPATH/SCRNSAVE_T1180_persistence.rb $t1180_path/SCRNSAVE_T1180_persistence.rb
-          fresh = "yes"
+          echo ${RedF}[x]${white} "Metasploit Post-module NOT found in msfdb."${Reset};
           sleep 2
+          echo ${BlueF}[*]${white} "Downloading Metasploit post-module from github"${Reset};
+          sleep 2
+          echo ""
+          sudo rm -f SCRNSAVE_T1180_persistence.rb
+          sudo wget https://raw.githubusercontent.com/r00t-3xp10it/resource_files/master/aux/SCRNSAVE_T1180_persistence.rb
+          echo ${BlueF}[*]${white} "Copy module to: $t1180_path/SCRNSAVE_T1180_persistence.rb"${Reset};
+          sleep 2
+          sudo cp $IPATH/SCRNSAVE_T1180_persistence.rb $t1180_path/SCRNSAVE_T1180_persistence.rb
+          fresh="yes"
+          count=$(( $count + 1 ))
        fi
 
-    echo ${BlueF}[*]${white} "Locate msfdb linux_hostrecon.rb path .."${Reset};
+    echo ${BlueF}[*]${white} "Query msfdb for linux_hostrecon.rb installation .."${Reset};
     Linux_path=$(locate modules/post/linux/gather | grep -v '\doc' | grep -v '\documentation' | head -n 1)
-    echo ${BlueF}[*]${white} "Path: $Linux_path/linux_hostrecon.rb"${Reset};
+    echo ${YellowF}[i]${white} "Path: $Linux_path/linux_hostrecon.rb"${Reset};
     sleep 2
 
        if [ -e "$Linux_path/linux_hostrecon.rb" ]; then
-          echo ${GreenF}[*]${white} "Post-module found in msfdb => ${GreenF}(no need to install)"${Reset};
+          echo ${GreenF}[*]${white} "Metasploit Post-module found in msfdb => ${GreenF}(no need to install)"${Reset};
           sleep 2
        else
-          echo ${BlueF}[*]${white} "Copy module to: $Linux_path/linux_hostrecon.rb"${Reset};
-          sudo cp $IPATH/linux_hostrecon.rb $Linux_path/linux_hostrecon.rb
-          fresh = "yes"
+          echo ${RedF}[x]${white} "Metasploit Post-module NOT found in msfdb."${Reset};
           sleep 2
+          echo ${BlueF}[*]${white} "Downloading Metasploit post-module from github"${Reset};
+          sleep 2
+          echo ""
+          sudo rm -f linux_hostrecon.rb
+          sudo wget https://raw.githubusercontent.com/r00t-3xp10it/resource_files/master/aux/linux_hostrecon.rb
+          echo ${BlueF}[*]${white} "Copy module to: $Linux_path/linux_hostrecon.rb"${Reset};
+          sleep 2
+          sudo cp $IPATH/linux_hostrecon.rb $Linux_path/linux_hostrecon.rb
+          fresh="yes"
+          count=$(( $count + 1 ))
        fi
 
 
     ## Updating msfdb
     if [ "$fresh" = "yes" ]; then
-       echo ${YellowF}[*]${white} "Updating msf database .."${Reset};
+       echo ${YellowF}[i]${white} "Completed: [ ${GreenF}$count${white} ] Module(s) Installation(s) .."${Reset};
+       sleep 1
+       echo ${BlueF}[*]${white} "Please wait, Updating msf database .."${Reset};
+       echo ""
        sudo service postgresql start
-       sudo msfdb reinit
-       sudo msfconsole -x 'db_status;reload_all;exit -y'
+       #sudo msfdb reinit
+       sudo msfconsole -q -x 'db_status;reload_all;exit -y'
+       echo ""
     fi
 
 
 
-    echo ${BlueF}[*]${white} "Locate nmap nse freevulnsearch.nse path .."${Reset};
-    sudo cp $IPATH/freevulnsearch.nse /usr/share/nmap/scripts/freevulnsearch.nse
-    echo ${BlueF}[*]${white} "Path: /usr/share/nmap/scripts/freevulnsearch.nse"${Reset};
+    echo ${BlueF}[*]${white} "query nmap nse freevulnsearch.nse installation .."${Reset};
     sleep 2
+    echo ${YellowF}[i]${white} "Path: /usr/share/nmap/scripts/freevulnsearch.nse"${Reset};
+    sleep 1
+    if [ -e "/usr/share/nmap/scripts/freevulnsearch.nse" ]; then
+       echo ${GreenF}[*]${white} "Nmap nse script found in database => ${GreenF}(no need to install)"${Reset};
+       sleep 2
+    else
+       echo ${RedF}[x]${white} "Nmap nse script NOT found in database."${Reset};
+       sleep 2
+       echo ${BlueF}[*]${white} "Downloading nmap nse script from github"${Reset};
+       sleep 2
+       echo ""
+       sudo rm -f freevulnsearch.nse
+       sudo wget https://raw.githubusercontent.com/OCSAF/freevulnsearch/master/freevulnsearch.nse
+       echo ${BlueF}[*]${white} "Copy module to: /usr/share/nmap/scripts/freevulnsearch.nse"${Reset};
+       sleep 2
+       sudo cp $IPATH/freevulnsearch.nse /usr/share/nmap/scripts/freevulnsearch.nse
+       echo ${YellowF}[i]${white} "Please wait, Updating nse database .."${Reset};
+       sudo nmap --script-updatedb
+    fi
 
-       if [ -e "/usr/share/nmap/scripts/freevulnsearch.nse" ]; then
-          echo ${GreenF}[*]${white} "Nmap nse script found in database => ${GreenF}(no need to install)"${Reset};
-          sleep 2
-       else
-          echo ${BlueF}[*]${white} "Copy module to: /usr/share/nmap/scripts/freevulnsearch.nse"${Reset};
-          sleep 2
-          echo ${YellowF}[*]${white} "Updating nse database .."${Reset};
-          sudo nmap --script-updatedb
-       fi
+
 
     echo ${BlueF}[*] "----------------------------------------"${Reset};
     echo ${BlueF}[*]${white} "All post-modules are ready to be used."${Reset};
@@ -126,6 +164,10 @@ else
     echo ${RedF}[x]${white} "Aborting tasks .."${Reset};
 
 fi
+
+    if [ "$fresh" = "yes" ]; then
+       sudo service postgresql stop
+    fi
 
 exit
 
